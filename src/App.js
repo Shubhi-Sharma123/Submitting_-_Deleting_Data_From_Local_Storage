061ca6859ae3e1164c2687e6566344cb4bf59294
Shubhi-Sharma123 <shubizz95@gmail.com>
2024-06-01 18:08:21 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState, useEffect } from "react";
 function App() {
+  let [data, setdata] = useState();
+  let [val, setval] = useState();
+  useEffect(() => {
+    const result = localStorage.getItem("mydata");
+    if (result) {
+      setdata(result);
+    }
+  }, []);
+
+  let handleclick = (e) => {
+    setval(e.target.value);
+  };
+
+  let submitdata = () => {
+    localStorage.setItem("mydata", val);
+    setdata(val);
+    setval("");
+  };
+  let deletedata = () => {
+    // localStorage.clear('mydata')
+    localStorage.removeItem("mydata");
+    setdata("");
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <input type="text" name="name" placeholder="Enter your value:" onChange={handleclick}/>
+      <br />
+      <br />
+      <button onClick={submitdata}>Add</button>
+      &nbsp;&nbsp;
+      <button onClick={deletedata}>Delete</button>
+      <h3>value = {data ? <p>{data}</p> : <p>no data</p>}</h3>
+    </>
   );
 }
 
